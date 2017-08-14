eb561efb324f80a130f6df99c9c21a78ddafd606
Steinarr134 <steinarrrrrrrrrr@gmail.com>
2017-08-14 16:43:25 +0000

    Updated examples to fit with changed syntax and other updates

--- examples/ReceivingData.py
+++ examples/ReceivingData.py
@@ -11,9 +11,9 @@ There are three ways to receive data:
 1. Using send_and_receive.
     We already covered this in the SendingData example
 
-2. By overwriting the MoteinoNetwork.receive function
+2. By creating a dedicated receive function for each node and binding it
 
-3. By writing a function and binding it to the Node
+3. By writing a default receive function and overwriting the network's default receiving function.
 
 """
 # So let's keep up with the SendingData example, I suggest reading that one first, if you haven't
@@ -24,53 +24,41 @@ There are three ways to receive data:
 #     } Payload;
 
 
-# We start by creating a our own subclass of the MoteinoNetwork. This
-# allows us to overwrite the default receive, ack and no_ack functions,
-# Let's call our class MyNetwork.
-class MyNetwork(MoteinoNetwork):
-    def __init__(self):
-        # Just like all good subclasses it starts by initializing the superclass.
-        # Here we'll also pass on the Serial port info
-        MoteinoNetwork.__init__(self, port='COM50')
+# Here you should start thinking about what you want to do with the network.
+# There are three functions that you might want to overwrite. Those are:
+# receive() - run if something is received from the network
+# ack()     - run if Base gets an ack after sending something
+# no_ack()  - run if Base sends something but doesn't receive an ack
+# These functions will be run in a new thread that you are allowed to hijack.
+# How cool is that?!
 
-    # Here you should start thinking about what you want to do with the network.
-    # There are three functions that you might want to overwrite. Those are:
-    # receive() - run if something is received from the network
-    # ack()     - run if Base gets an ack after sending something
-    # no_ack()  - run if Base sends something but doesn't receive an ack
-    # These functions will be run in a new thread that you are allowed to hijack.
-    # How cool is that?! (be careful though. If you make the threads run something
-    # time consuming you could end up with too many threads running and some nasty
-    # stuff might happen, or not.... I'm no expert. But they probably do
-    # implement thread pools for a reason....)
+# Here are those functions as they look by default in the source code:
 
-    # Here are those functions as they look by default in the source code:
+def default_receive(diction):
+    """
+    The default fucntion called when network receives something
+    :param diction: dict
+    """
+    print("MoteinoNetwork received: " + str(diction) + " from " + diction['SenderName'])
 
-    def receive(self, sender, diction):
-        """
-        User should overwrite this function
-        :param sender: Device
-        :param diction: dict
-        """
-        print("MoteinoNetwork received: " + str(diction) + " from " + sender.Name)
 
-    def no_ack(self, sender, last_sent_diction):
-        """
-        User might want to overwrite this function
-        :param sender: Device
-        :param last_sent_diction: dict
-        """
-        print("Oh no! We didn't recieve an ACK from " + sender.Name + " when we sent " + str(last_sent_diction))
+def default_no_ack(last_sent_diction):
+    """
+    The default fucntion called when network receives no ack after sending something
+    :param last_sent_diction: dict
+    """
+    sender = last_sent_diction['Sender']
+    print("Oh no! We didn't recieve an ACK from " + sender.Name + " when we sent " + str(last_sent_diction))
 
-    def ack(self, sender, last_sent_diction):
-        """
-        This function is totally unnecessary.... mostly for debugging but maybe
-        it will be useful someday to overwrite this with something
-        :param sender: Device
-        :param last_sent_diction: dict
-        """
-        if self.print_when_acks_recieved:
-            print(sender.Name + " responded with an ack when we sent: " + str(last_sent_diction))
+
+def default_ack(last_sent_diction):
+    """
+    The default fucntion called when network receives an ack after sending something.
+    This function is essentially unnecessary.... mostly for debugging but maybe
+    it will be useful someday to overwrite this with something
+    :param last_sent_diction: dict
+    """
+    pass
 
 
 # Now you might be wandering what those dictions are... diction is my made up name for python's
@@ -80,10 +68,10 @@ class MyNetwork(MoteinoNetwork):
 
 
 # First up, instantiate your network and define our TestNode
-mynetwork = MyNetwork()
-TestNode = mynetwork.add_node('TestNode', 10, "int info;" + "int numbers[5];")
+mynetwork = MoteinoNetwork(False)
+TestNode = mynetwork.add_node(10, "int info;" + "int numbers[5];", 'TestNode')
 
-# By default, If a node sends something the MoteinoNetwork.receive function will be called but
+# By default, If a node sends something the default_receive function will be called but
 # we can also define a new function for each node:
 
 
@@ -97,7 +85,12 @@ def test_node_receive(diction):
 TestNode.bind(receive=test_node_receive)
 
 # Now, every time TestNode sends us something, test_node_receive will be run instead of
-# mynetwork.receive.
+# default_receive
+
+# We can also change the default functions:
 
 
+def my_no_ack(last_sent_diction):
+    print("No ack when sending: " + str(last_sent_diction))
 
+mynetwork.bind_default(no_ack=my_no_ack)
--- examples/SendingData.py
+++ examples/SendingData.py
@@ -7,6 +7,10 @@ How to send data
 """
 
 from moteinopy import MoteinoNetwork
+import logging
+
+# let's set the logging level to debug, this way we can better see what is happening during runtime.
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
 
@@ -14,7 +18,13 @@ if __name__ == "__main__":
     # so they can still be run individually.
 
     # First let's instantiate our network:
-    mynetwork = MoteinoNetwork('COM50')
+    # Check out the InitiatingNetwork example for more on this.
+
+    # Passing '' or False will make the module skip the base connection,
+    # this will of course provide no functionality but is useful for running these
+    # examples if you don't have a base present. Anything you throw at a node will
+    # just come right back at you, 50ms later.
+    mynetwork = MoteinoNetwork(False)
 
     # For the sake of this example I am imagining a device called TestNode.
     # It is on the network and it has NODEID=10. It is expecting a struct
@@ -27,12 +37,12 @@ if __name__ == "__main__":
     example = 1
 
     if example == 1:  # starting off easy
-        # scenario:    We want to send info=5 and numbers = {1,2,3,4,5}
+        # scenario:    We want to send info=5 and numbers={1,2,3,4,5}
 
         # First we add the device to the network, like so:
-        mynetwork.add_node(name='TestNode',
-                           _id=10,  # I had to underscore id to prevent naming conflict :/
-                           structstring="int info;" + "int numbers[5];")
+        mynetwork.add_node(_id=10,  # I had to underscore id to prevent naming conflict :/
+                           structstring="int info;" + "int numbers[5];",
+                           name='TestNode',)
 
         # Now we can send data using the send method, you can for example pass
         # keyword arguments matching what you defined in the structstring (case sensitive)
@@ -40,10 +50,10 @@ if __name__ == "__main__":
 
     elif example == 2:  # using the Device class and *args
 
-        # scenario:     We want to send info=5 and numbers = {1,2,3,4,5} (alternative)
+        # scenario:     We want to send info=5 and numbers={1,2,3,4,5} (alternative)
 
         # We can also add to network and get a handle to the node.
-        TestNode = mynetwork.add_node('TestNode', 10, "int info;" + "int numbers[5];")
+        TestNode = mynetwork.add_node(10, "int info;" + "int numbers[5];", 'TestNode')
 
         # We can also pass it arguments in the same order as the structstring
         TestNode.send(5, [1, 2, 3, 4, 5])
@@ -53,7 +63,9 @@ if __name__ == "__main__":
         # must send it a request and then wait while the Node gathers that info and
         # sends it to us.
 
-        TestNode = mynetwork.add_node('TestNode', 10, "int info;" + "int numbers[5];")
+        TestNode = mynetwork.add_node(10, "int info;" + "int numbers[5];")
+        # by skipping the name parameter the module will assign a default name: Node-(id)
+        # in this case: Node-10
 
         # For this we can use the send_and_receive function. This function sends the data
         # just like send does but then waits for the node to respond. When the node responds
@@ -63,7 +75,7 @@ if __name__ == "__main__":
 
         # Btw, you may have noticed that we didn't specify 'numbers'. That is ok, the module will
         # assume that it is zero. We also specified the max wait time (in milliseonds) if this is
-        # not specified the default wait time will be used. You can adjust the default with
+        # not specified the default wait time (500ms) will be used. You can adjust the default with
         # mynetwork.max_wait
 
         # If something fails and we don't get a response then Response will be None
@@ -72,19 +84,30 @@ if __name__ == "__main__":
         else:
             print("It didn't work, we got nothing :(")
 
-    elif example == 4:  # using the translation to make code more readable
+    elif example == 4:  # using the translation service to make code more readable
 
-        TestNode = mynetwork.add_node('TestNode', 10, "int info;" + "int numbers[5];")
+        TestNode = mynetwork.add_node(10, "int info;" + "int numbers[5];", 'TestNode')
 
         # The module has a built in translation service, let's redo our example number 3
         # using the translation service.
 
-        TestNode.add_translation('info', ('SendNumbers', 123))
+        TestNode.add_translation('info',
+                                 ('SendMeNumbers', 123),
+                                 ('HereAreTheNumbers', 124))
+        # The syntax is add_translation(part, *args) where args are tuples of what should be
+        # translated into what
 
         # And now we can use :
-        TestNode.send_and_receive(info='SendNumbers', max_wait=2000)
+        Response = TestNode.send_and_receive('SendNumbers')
 
-        # The only purpose for this translation service is making your code more readable
+        if not Response:
+            print("Oh, no!, no Response :( ")
+        else:
+            if Response['info'] == "HereAreTheNumbers":
+                print(Response["numbers"])
+
+        # The only purpose for this translation service is making your code more readable,
+        # If it is confusing you then you can safely just not use it.
 
 
 # That's all for now folks, I'll add some more examples if requested
